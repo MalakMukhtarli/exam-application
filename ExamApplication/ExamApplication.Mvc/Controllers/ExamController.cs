cc1c31084c4caee231f6463cc03814be8646cc0d
MalakMukhtarli <malakem@code.edu.az>
2024-07-03 18:49:02 +0400

    added Grade page

---
+++ ExamApplication/ExamApplication.Mvc/Controllers/ExamController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExamApplication.Mvc.Controllers;
+
+public class ExamController : Controller
+{
+    
+}
---
+++ ExamApplication/ExamApplication.Mvc/Controllers/GradeController.cs
@@ -0,0 +1,21 @@
+﻿using ExamApplication.Business.Services.Grades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamApplication.Mvc.Controllers;
+
+public class GradeController : Controller
+{
+    private readonly IGradeService _gradeService;
+
+    public GradeController(IGradeService gradeService)
+    {
+        _gradeService = gradeService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await _gradeService.GetAll();
+        
+        return View(response);
+    }
+}
