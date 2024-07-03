ac1f9ffcb4d28ba8c1b15d6fdaec97de4e787bae
MalakMukhtarli <malakem@code.edu.az>
2024-07-03 18:53:03 +0400

    added pupil page

--- ExamApplication/ExamApplication.Mvc/Controllers/PupilController.cs
+++ ExamApplication/ExamApplication.Mvc/Controllers/PupilController.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ExamApplication.Business.Services.Pupils;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExamApplication.Mvc.Controllers;
 
 public class PupilController : Controller
 {
-    
+    private readonly IPupilService _pupilService;
+
+    public PupilController(IPupilService pupilService)
+    {
+        _pupilService = pupilService;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var response = await _pupilService.GetAll();
+        
+        return View(response);
+    }
 }
