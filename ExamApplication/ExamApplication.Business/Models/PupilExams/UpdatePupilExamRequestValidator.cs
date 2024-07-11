fd593f0ef83725566707d1489d1b7d11ec21e6ca
MalakMukhtarli <malakem@code.edu.az>
2024-07-11 09:43:46 +0400

    fix UpdatePupilExamRequestValidator

--- ExamApplication/ExamApplication.Business/Models/PupilExams/UpdatePupilExamRequestValidator.cs
+++ ExamApplication/ExamApplication.Business/Models/PupilExams/UpdatePupilExamRequestValidator.cs
@@ -9,7 +9,7 @@ public class UpdatePupilExamRequestValidator : BaseValidator<UpdatePupilExamRequ
         RuleFor(e => e.PupilExamId)
             .NotEmpty().WithMessage("Şagird boş ola bilməz");
         RuleFor(e => e.Mark)
-            .NotNull().WithMessage("Dərs üçün sinif seçilməlidir")
-            .Must(w => w.ToString().Length <= 1).WithMessage("Qiymətin uzunluğu ən çox 1 ola bilər");
+            .NotNull().WithMessage("Qiymət boş ola bilməz")
+            .Must(w => w is >= 1 and <= 5).WithMessage("Qiymət 1 ilə 5 arasında ola bilər");
     }
 }
